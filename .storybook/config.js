20fae82631d06ef0bc8087e55cf3b602e93e9b3d
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-26 15:14:53 -0300

    Install storyshot for automated testing, and configure storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// configure(require.context('../src/', true, /\.stories\.js$/), module);
+
+// With this one, I thin you have to also have the Module.js file
+//
+// const req = require.context('../src', true, /\.stories.js$/);
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+configure(loadStories, module);
