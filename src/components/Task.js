ef8e546db1db63f0aefbf8467947358e2b62966a
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-27 12:30:16 -0300

    Knobs addon, and use it in Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 // Simply taking in the attributes we know we’ll need and the two actions
 // you can take on a task (to move it between lists):
 // This combined with index.css, yields some UI
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === 'TASK_ARCHIVED'}
+          defaultChecked={state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
+        />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
-        {state !== 'TASK_ARCHIVED' && (
+        {state !== "TASK_ARCHIVED" && (
           <a onClick={() => onPinTask(id)}>
             <span className={`icon-star`} />
           </a>
@@ -35,8 +45,8 @@ Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
   }),
   onArchiveTask: PropTypes.func,
-  onPinTask: PropTypes.func,
+  onPinTask: PropTypes.func
 };
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
-import Task from './Task';
+import Task from "./Task";
 
 export const task = {
-  id: '1',
-  title: 'Test Task',
-  state: 'TASK_INBOX',
-  updatedAt: new Date(2018, 0, 1, 9, 0),
+  id: "1",
+  title: "Test Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
 // action() allows us to create a callback that appears in the actions panel of
@@ -17,9 +18,11 @@ export const task = {
 // <Task {...actions}> is equivalent to
 // <Task onPinTask={actions.onPinTask} onArchiveTask={actions.onArchiveTask}>.
 export const actions = {
-  onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask'),
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
 };
+
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
 
 // There are two basic levels of organization in Storybook:
 // the component and its child stories.
@@ -31,7 +34,21 @@ export const actions = {
 // To define our stories, we call add() once for each of our test states to generate a story.
 // Returns a rendered element (i.e. a component class with a set of props) in a given state
 // --exactly like a React Stateless Functional Component
-storiesOf('Task', module)
-  .add('default', () => <Task task={task} {...actions} />)
-  .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    // the object knob type accepts a label and a default object as parameters.
+    // The label is constant and shows up to the left of a text field in your addons panel
+    // The object you've passed will be represented as an editable JSON blob.
+    // As long as you submit valid JSON, your component will adjust based upon the data being passed to the object
+    <Task task={object("task", { ...task })} {...actions} />
+  ))
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+  ))
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ))
+  .add("long title", () => (
+    <Task task={{ ...task, title: longTitle }} {...actions} />
+  ));
